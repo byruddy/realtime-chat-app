24b78c5f0e6681bd26a1e3b93ee3f0836747f0d5
byruddy <rudihikmatullah@outlook.com>
2021-07-12 08:59:11 +0700

    Create users page dinamyc

--- users.php
+++ users.php
@@ -1,22 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Realtime Chat App | CodingNepal</title>
-    <link rel="stylesheet" href="style/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-</head>
+<?php
+    session_start();
+    if (!isset($_SESSION['unique_id'])) {
+        header("Location: login.php");
+    }
+    include_once "header.php" ?>
 <body>
     <div class="wrapper">
         <section class="users">
            <header>
+            <?php  
+                include_once "php/config.php";
+                $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = '{$_SESSION['unique_id']}'");
+                if (mysqli_num_rows($sql) > 0) {
+                    $row = mysqli_fetch_assoc($sql);
+                }
+            ?>
                <div class="content">
-                    <img src="style/img.png" alt="">
+                    <img src="php/images/<?= $row['img']; ?>" alt="">
                     <div class="details">
-                        <span>Coding Nepal</span>
-                        <p>Active now</p>
+                        <span><?= $row['fname'].' '.$row['lname']; ?></span>
+                        <p><?= $row['status']; ?></p>
                     </div>
                </div>
                <a href="#" class="logout">Logout</a>
@@ -27,66 +30,6 @@
                <button><i class="fas fa-search"></i></button>
             </div>
             <div class="users-list">
-                <a href="#">
-                    <div class="content">
-                        <img src="style/img.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="style/img.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="style/img.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="style/img.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="style/img.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
-                <a href="#">
-                    <div class="content">
-                        <img src="style/img.png" alt="">
-                        <div class="details">
-                            <span>Coding Nepal</span>
-                            <p>This is test message</p>
-                        </div>
-                    </div>
-                    <div class="status-dot"><i class="fas fa-circle"></i></div>
-                </a>
             </div>
         </section>
     </div>
